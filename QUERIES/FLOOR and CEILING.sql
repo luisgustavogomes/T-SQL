4dac6eed62749c24ead2d84b36848bcf2b2cdf27
luisgustavogomes <luisgustavogomes@outlook.com>
2019-10-28 22:25:34 -0300

    Upload

---
+++ QUERIES/FLOOR and CEILING.sql
@@ -0,0 +1,29 @@
+SELECT 'FLOOR(3.63)   ' AS COMANDO , FLOOR(3.63)    Result
+
+     , 'FLOOR(500.71) ' AS COMANDO , FLOOR(500.71)  Result
+
+     , 'FLOOR(-3.63)  ' AS COMANDO , FLOOR(-3.63)   Result
+
+     , 'FLOOR(-500.71)' AS COMANDO , FLOOR(-500.71) Result
+
+/*   <-----      Linha           */
+
+/*  -4,-3,-2,-1, 0 , 1, 2, 3, 4  */
+
+ 
+
+ 
+
+SELECT 'CEILING(3.63)   ' AS COMANDO , CEILING(3.63)    Result
+
+     , 'CEILING(500.71) ' AS COMANDO , CEILING(500.71)  Result
+
+     , 'CEILING(-3.63)  ' AS COMANDO , CEILING(-3.63)   Result
+
+     , 'CEILING(-500.71)' AS COMANDO , CEILING(-500.71) Result
+
+ 
+
+/*          Linha   ----->       */
+
+/*  -4,-3,-2,-1, 0 , 1, 2, 3, 4  */
