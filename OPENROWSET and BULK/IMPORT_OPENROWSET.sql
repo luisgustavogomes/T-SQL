bbaf0ba191873ba17ecaf98ac3124c0bf2d747f2
luisgustavogomes <luisgustavogomes@outlook.com>
2019-11-18 07:52:20 -0300

    Upload

---
+++ OPENROWSET and BULK/IMPORT_OPENROWSET.sql
@@ -0,0 +1,10 @@
+USE Northwind
+GO
+
+--https://www.dirceuresende.com/blog/sql-server-como-importar-arquivos-de-texto-para-o-banco-ole-automation-clr-bcp-bulk-insert-openrowset/
+
+SELECT * 
+FROM OPENROWSET (
+
+
+)
